07092c6f6e01bc6f3176bbbdc90fe8880236f3f4
Scott Brumley <scott@nsisecure.com>
2020-07-07 19:13:29 -0400

    Fixed unauthorized issue

--- main.go
+++ main.go
@@ -16,13 +16,13 @@ func main() {
 	for {
 		select {
 		case procID = <-c:
-			fmt.Println("Sending: " + procID + " " + time.Now().Format(time.RFC850))
+			fmt.Println(time.Now().Format(time.RFC850) + " Sending: " + procID)
 			continue
 		case <-timeout:
 			fmt.Println("timeout")
 			break
 		}
-		pingCMD := "{\"commands\": [\"https://192.168.192.185/api/vw6OE0D7kDffGeMpA5JXhHuQZaXMtX8Jh8zcEyyb/sensors\",\"https://192.168.192.56/api/514oY5iuhkg4lFgm0iL6qRlCIAAqvA3y/sensors\",\"https://192.168.192.58/api/Yj3knZa5VWYGLYo6n7TAOVWrRW-3VK9Un1UALd9t/sensors\"]}"
+		pingCMD := "{\"commands\": [\"https://192.168.192.185/api/vw6OE0D7kDffGeMpA5JXhHuQZaXMtX8Jh8zcEyyb/sensors\",\"https://192.168.192.56/api/OdZrhUY-514oY5iuhkg4lFgm0iL6qRlCIAAqvA3y/sensors\",\"https://192.168.192.58/api/Yj3knZa5VWYGLYo6n7TAOVWrRW-3VK9Un1UALd9t/sensors\"]}"
 		PublishCtl(client, procID, pingCMD)
 		timeout = time.After(60 * time.Second)
 	}
