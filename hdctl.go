5f7fb1841311d141ca27f083b3a2bf4c526f7302
scott <scott@nsisecure.com>
2020-07-22 19:13:09 -0400

    Refactoring to include more structures in liu of objects

--- hdctl.go
+++ hdctl.go
@@ -1,248 +1,66 @@
 package main
 
 import (
-	"context"
-	"crypto/rand"
 	"encoding/json"
 	"fmt"
-	"github.com/eclipse/paho.mqtt.golang"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"io/ioutil"
-	"log"
-	"os"
-	"regexp"
-	"strconv"
+	"hdctl"
 	"strings"
 	"time"
 )
 
-// Helper Functions
-func ValidIP4(ipAddress string) bool {
-	ipAddress = strings.Trim(ipAddress, " ")
-	re, _ := regexp.Compile(`^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`)
+func main() {
+	controlCenter := hdctl.New("config.json")
 
-	// Is a valid IP Address
-	if re.MatchString(ipAddress) {
-		// Test the octet ranges
-		octets := strings.Split(ipAddress, ".")
+	for {
+		select {
+		case msgStr := <-controlCenter.CtrlMessages:
+			if json.Valid([]byte(msgStr)) {
+				procID, hubid, action, command, retval := hdctl.ReadCtrl(msgStr)
+				//procID, action, command, result := readCtrl(msgStr)
+				// If intiation strings match then return configuration
+				if action == "initiate" {
+					fmt.Println(procID + " Initiated")
+					fmt.Println(time.Now().Format(time.RFC850) + " hacmd/ctrl just checked in.")
+					pingCMD := "{\"procid\":\"" + procID + "\",\"commands\": [{\"url\": \"https://192.168.192.55/api/config\",\"hubid\": \"001788FFFE277094\"},{\"url\": \"https://192.168.192.56/api/config\",\"hubid\": \"ECB5FAFFFE10C52F\"},{\"url\": \"https://192.168.192.58/api/config\",\"hubid\": \"ECB5FAFFFE0DA7C7\"}]}"
+					go controlCenter.SendCommands(pingCMD)
+				}
+				if action == "result" {
+					commandStr := strings.Split(command, "/")
+					switch len(commandStr) {
+					//Priviledged Commands
+					case 6:
+						switch commandStr[5] {
+						case "sensors":
+							// Update the Sensor Details
+							controlCenter.UpdateSensors(hubid, retval)
+							//hdctl.updateOne_mongoDB(mongoClient, "homeSysDB", "sensors", hubid, retval)
+						case "config":
+							// Update Configuration Database
 
-		for i := range octets {
-			octet, err := strconv.Atoi(octets[i])
-			if err != nil {
-				log.Fatalln(err)
+						}
+					//Base Commands
+					case 5:
+						switch commandStr[4] {
+						case "config":
+							// Update Configuration Database
+
+						}
+					}
+
+				}
+			} else {
+				fmt.Println("Not a JSON String")
+				continue
 			}
 
-			if octet > 255 {
-				return false
-			}
-			if octet < 0 {
-				return false
+		//Issue Sensor Commands
+		case <-time.After(60 * time.Second):
+			fmt.Println("Check Jobs")
+			//results := hdctl.find_mongoDB(mongoClient, "homeSysDB", "jobs", brokerID)
+			results := controlCenter.FindJobs(controlCenter.ProcID)
+			for _, job := range results {
+				go controlCenter.SendCommands(job)
 			}
 		}
-		return true
 	}
-
-	return false
-}
-func validHost(host string) bool {
-	host = strings.Trim(host, " ")
-	re, _ := regexp.Compile(`^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$`)
-	if re.MatchString(host) {
-		return true
-	}
-	return false
-}
-
-//define a function for the default message handler
-var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-	fmt.Printf("TOPIC: %s\n", msg.Topic())
-	fmt.Printf("MSG: %s\n", msg.Payload())
-}
-
-type jobsStruct struct {
-	JobID    int        `json:"jobid"`
-	Trigger  string     `json:"trigger"`
-	ProcID   string     `json:"procid"`
-	Commands []Commands `json:"commands"`
-}
-type Commands struct {
-	URL   string `json:"url"`
-	Hubid string `json:"hubid"`
-}
-
-type hacmdInit struct {
-	ProcID  string                 `json:"procid"`
-	HubID   string                 `json:"hubid"`
-	Action  string                 `json:"action"`
-	Command string                 `json:"command"`
-	Results map[string]interface{} `json:"results"`
-}
-
-// Main Functions
-func readCtrl(configstr string) (procID string, hubid string, action string, command string, results map[string]interface{}) {
-	res := hacmdInit{}
-	err := json.Unmarshal([]byte(configstr), &res)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	procID = res.ProcID
-	hubid = res.HubID
-	action = res.Action
-	command = res.Command
-	results = res.Results
-	return
-}
-func ReadConfig(configstr string) (user string, pass string, broker string, procID, mongoDB string) {
-	plan, _ := ioutil.ReadFile(configstr)
-	var data map[string]interface{}
-	err := json.Unmarshal(plan, &data)
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-	user = data["user"].(string)
-	pass = data["pass"].(string)
-	broker = data["broker"].(string)
-	procID = data["procID"].(string)
-	mongoDB = data["mongoDB"].(string)
-	return
-}
-func ProcUUID() (uuid string) {
-	b := make([]byte, 16)
-	_, err := rand.Read(b)
-	if err != nil {
-		log.Fatal(err)
-	}
-	uuid = fmt.Sprintf("%x-%x-%x-%x-%x",
-		b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
-	return
-}
-func connect_mqtt(broker string, user string, pass string) (client mqtt.Client) {
-
-	//create a ClientOptions struct setting the broker address, clientid, turn
-	//off trace output and set the default message handler
-	opts := mqtt.NewClientOptions().AddBroker("tcp://" + broker)
-	opts.SetClientID(ProcUUID())
-	opts.SetDefaultPublishHandler(f)
-	opts.SetUsername(user)
-	opts.SetPassword(pass)
-	//create and start a client using the above ClientOptions
-	client = mqtt.NewClient(opts)
-	if token := client.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
-
-	return
-
-}
-func connect_mongoDB(mongoDB string) (client *mongo.Client) {
-	//Set up a context required by mongo.Connect
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	//To close the connection at the end
-	defer cancel()
-	//We need to set up a client first
-	//It takes the URI of your database
-	client, error := mongo.NewClient(options.Client().ApplyURI(mongoDB))
-	if error != nil {
-		log.Fatal(error)
-	}
-	//Call the connect function of client
-	error = client.Connect(ctx)
-	//Checking the connection
-	error = client.Ping(context.TODO(), nil)
-	fmt.Println("Database connected")
-	return
-}
-func updateOne_mongoDB(mongoClient *mongo.Client, dbStr string, collectionStr string, hubid string, retval map[string]interface{}) {
-	//Set up a context required by mongo.Connect
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	//To close the connection at the end
-	defer cancel()
-	// Update Sensor Database
-	SensorsCollection := mongoClient.Database(dbStr).Collection(collectionStr)
-	filter := bson.D{{"hubid", hubid}}
-	// Need to specify the mongodb output operator too
-	newName := bson.D{
-		{"$set", bson.D{
-			{collectionStr, retval},
-		}},
-	}
-	res, err := SensorsCollection.UpdateOne(ctx, filter, newName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	updatedObject := *res
-	fmt.Printf("Modified count is : %d", updatedObject.ModifiedCount)
-	//fmt.Printf("The matched count is : %d, the modified count is : %d", updatedObject.MatchedCount, updatedObject.ModifiedCount)
-
-}
-func find_mongoDB(mongoClient *mongo.Client, dbStr string, collectionStr string, procid string) (results []string) {
-	//Set up a context required by mongo.Connect
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	//To close the connection at the end
-	defer cancel()
-
-	// Collection to retrieve from
-	Collection := mongoClient.Database(dbStr).Collection(collectionStr)
-
-	cur, error := Collection.Find(ctx, bson.D{{"procid", procid}})
-	var alljobs []*jobsStruct
-	//Loops over the cursor stream and appends to result array
-	for cur.Next(context.TODO()) {
-		var jobResult jobsStruct
-		err := cur.Decode(&jobResult)
-		if err != nil {
-			log.Fatal(error)
-		}
-		alljobs = append(alljobs, &jobResult)
-	}
-	//dont forget to close the cursor
-	defer cur.Close(context.TODO())
-
-	for _, element := range alljobs {
-		res2B, _ := json.Marshal(element)
-		results = append(results, string(res2B))
-	}
-
-	return
-}
-func SubscribeTo(topic string, client mqtt.Client, c chan string) {
-	if token := client.Subscribe(topic, 0, func(client mqtt.Client, msg mqtt.Message) {
-		payloadStr := string(msg.Payload())
-		//time.Sleep(6 * time.Second)
-		// Get ProcID from Topic
-		c <- payloadStr
-
-		/*
-			if len(payloadStr) > 0 {
-				c <- payloadStr
-			}
-		*/
-
-		return
-	}); token.Wait() && token.Error() != nil {
-		return
-	}
-	return
-}
-func Unsubscribe(client mqtt.Client) {
-	// Unscribe
-	if token := client.Unsubscribe("hacmd/#"); token.Wait() && token.Error() != nil {
-		fmt.Println(token.Error())
-		os.Exit(1)
-	}
-}
-func Disconnect(client mqtt.Client) {
-	// Disconnect
-	client.Disconnect(250)
-	time.Sleep(1 * time.Second)
-}
-func PublishTo(topic string, client mqtt.Client, msg string) {
-	// Send Configuration
-	fmt.Println(time.Now().Format(time.RFC850) + " Sending command " + msg + " to " + topic)
-	// Publish Response
-	token := client.Publish(topic, 0, false, msg)
-	token.Wait()
 }
---
+++ src/hdctl/hdctl.go
@@ -0,0 +1,283 @@
+package hdctl
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/json"
+	"fmt"
+	"github.com/eclipse/paho.mqtt.golang"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// Helper Functions
+func ValidIP4(ipAddress string) bool {
+	ipAddress = strings.Trim(ipAddress, " ")
+	re, _ := regexp.Compile(`^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`)
+
+	// Is a valid IP Address
+	if re.MatchString(ipAddress) {
+		// Test the octet ranges
+		octets := strings.Split(ipAddress, ".")
+
+		for i := range octets {
+			octet, err := strconv.Atoi(octets[i])
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			if octet > 255 {
+				return false
+			}
+			if octet < 0 {
+				return false
+			}
+		}
+		return true
+	}
+
+	return false
+}
+func validHost(host string) bool {
+	host = strings.Trim(host, " ")
+	re, _ := regexp.Compile(`^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$`)
+	if re.MatchString(host) {
+		return true
+	}
+	return false
+}
+
+//define a function for the default message handler
+var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	fmt.Printf("MSG: %s\n", msg.Payload())
+}
+
+type jobsStruct struct {
+	JobID    int        `json:"jobid"`
+	Trigger  string     `json:"trigger"`
+	ProcID   string     `json:"procid"`
+	Commands []Commands `json:"commands"`
+}
+type Commands struct {
+	URL       string `json:"url"`
+	Hubid     string `json:"hubid"`
+	VendorTag string `json:"vendortag"`
+}
+
+type hacmdInit struct {
+	ProcID  string                 `json:"procid"`
+	HubID   string                 `json:"hubid"`
+	Action  string                 `json:"action"`
+	Command string                 `json:"command"`
+	Results map[string]interface{} `json:"results"`
+}
+
+type hactl struct {
+	ProcID       string
+	CtrlMessages chan string
+	mqttClient   mqtt.Client
+	mongoClient  *mongo.Client
+}
+
+func New(configStr string) hactl {
+	ch1 := make(chan string)
+	user, pass, broker, procID, mongoDB := ReadConfig("config.json")
+	mqttClient := connect_mqtt(broker, user, pass)
+	mongoClient := connect_mongoDB(mongoDB)
+
+	//Checking the connection
+	mongoClient.Ping(context.TODO(), nil)
+	fmt.Println("Database connected")
+
+	// Subscribe to a topic
+	go SubscribeTo("hacmd/ctrl", mqttClient, ch1)
+	ctrlProc := hactl{procID, ch1, mqttClient, mongoClient}
+	return ctrlProc
+}
+
+// Main Functions
+func (controlCenter hactl) SendCommands(msg string) {
+	topic := "hacmd/cmd"
+	publishTo(topic, controlCenter.mqttClient, msg)
+}
+func (controlCenter hactl) UpdateSensors(hubid string, sensorVal map[string]interface{}) {
+	updateOne_mongoDB(controlCenter.mongoClient, "homeSysDB", "sensors", hubid, sensorVal)
+}
+func (controlCenter hactl) FindJobs(brokerId string) (results []string) {
+	results = find_mongoDB(controlCenter.mongoClient, "homeSysDB", "jobs", brokerId)
+	return results
+}
+func ReadCtrl(configstr string) (procID string, hubid string, action string, command string, results map[string]interface{}) {
+	res := hacmdInit{}
+	err := json.Unmarshal([]byte(configstr), &res)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	procID = res.ProcID
+	hubid = res.HubID
+	action = res.Action
+	command = res.Command
+	results = res.Results
+	return
+}
+func ReadConfig(configstr string) (user string, pass string, broker string, procID, mongoDB string) {
+	plan, _ := ioutil.ReadFile(configstr)
+	var data map[string]interface{}
+	err := json.Unmarshal(plan, &data)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	user = data["user"].(string)
+	pass = data["pass"].(string)
+	broker = data["broker"].(string)
+	procID = data["procID"].(string)
+	mongoDB = data["mongoDB"].(string)
+	return
+}
+func ProcUUID() (uuid string) {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+	uuid = fmt.Sprintf("%x-%x-%x-%x-%x",
+		b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+	return
+}
+func connect_mqtt(broker string, user string, pass string) (client mqtt.Client) {
+
+	//create a ClientOptions struct setting the broker address, clientid, turn
+	//off trace output and set the default message handler
+	opts := mqtt.NewClientOptions().AddBroker("tcp://" + broker)
+	opts.SetClientID(ProcUUID())
+	opts.SetDefaultPublishHandler(f)
+	opts.SetUsername(user)
+	opts.SetPassword(pass)
+	//create and start a client using the above ClientOptions
+	client = mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	return
+
+}
+func connect_mongoDB(mongoDB string) (client *mongo.Client) {
+	//Set up a context required by mongo.Connect
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	//To close the connection at the end
+	defer cancel()
+	//We need to set up a client first
+	//It takes the URI of your database
+	client, error := mongo.NewClient(options.Client().ApplyURI(mongoDB))
+	if error != nil {
+		log.Fatal(error)
+	}
+	//Call the connect function of client
+	error = client.Connect(ctx)
+	//Checking the connection
+	error = client.Ping(context.TODO(), nil)
+	fmt.Println("Database connected")
+	return
+}
+func updateOne_mongoDB(mongoClient *mongo.Client, dbStr string, collectionStr string, hubid string, retval map[string]interface{}) {
+	//Set up a context required by mongo.Connect
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	//To close the connection at the end
+	defer cancel()
+	// Update Sensor Database
+	SensorsCollection := mongoClient.Database(dbStr).Collection(collectionStr)
+	filter := bson.D{{"hubid", hubid}}
+	// Need to specify the mongodb output operator too
+	newName := bson.D{
+		{"$set", bson.D{
+			{collectionStr, retval},
+		}},
+	}
+	res, err := SensorsCollection.UpdateOne(ctx, filter, newName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	updatedObject := *res
+	fmt.Printf("Modified count is : %d", updatedObject.ModifiedCount)
+	//fmt.Printf("The matched count is : %d, the modified count is : %d", updatedObject.MatchedCount, updatedObject.ModifiedCount)
+
+}
+func find_mongoDB(mongoClient *mongo.Client, dbStr string, collectionStr string, procid string) (results []string) {
+	//Set up a context required by mongo.Connect
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	//To close the connection at the end
+	defer cancel()
+
+	// Collection to retrieve from
+	Collection := mongoClient.Database(dbStr).Collection(collectionStr)
+
+	cur, error := Collection.Find(ctx, bson.D{{"procid", procid}})
+	var alljobs []*jobsStruct
+	//Loops over the cursor stream and appends to result array
+	for cur.Next(context.TODO()) {
+		var jobResult jobsStruct
+		err := cur.Decode(&jobResult)
+		if err != nil {
+			log.Fatal(error)
+		}
+		alljobs = append(alljobs, &jobResult)
+	}
+	//dont forget to close the cursor
+	defer cur.Close(context.TODO())
+
+	for _, element := range alljobs {
+		res2B, _ := json.Marshal(element)
+		results = append(results, string(res2B))
+	}
+
+	return
+}
+func SubscribeTo(topic string, client mqtt.Client, c chan string) {
+	if token := client.Subscribe(topic, 0, func(client mqtt.Client, msg mqtt.Message) {
+		payloadStr := string(msg.Payload())
+		//time.Sleep(6 * time.Second)
+		// Get ProcID from Topic
+		c <- payloadStr
+
+		/*
+			if len(payloadStr) > 0 {
+				c <- payloadStr
+			}
+		*/
+
+		return
+	}); token.Wait() && token.Error() != nil {
+		return
+	}
+	return
+}
+func Unsubscribe(client mqtt.Client) {
+	// Unscribe
+	if token := client.Unsubscribe("hacmd/#"); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+}
+func Disconnect(client mqtt.Client) {
+	// Disconnect
+	client.Disconnect(250)
+	time.Sleep(1 * time.Second)
+}
+func publishTo(topic string, client mqtt.Client, msg string) {
+	// Send Configuration
+	fmt.Println(time.Now().Format(time.RFC850) + " Sending command " + msg + " to " + topic)
+	// Publish Response
+	token := client.Publish(topic, 0, false, msg)
+	token.Wait()
+}
